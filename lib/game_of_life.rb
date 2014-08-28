ae7ba5e49c051d46e3117a2256dc54ff0dee12ba
Jimmy Börjesson <lagginglion@gmail.com>
2014-08-28 22:29:06 +0200

    Refactored GameOfLife and made it stop starting a blank window before run is called

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -4,14 +4,14 @@ require './lib/gosu_runner'
 
 class GameOfLife
 
-  attr_reader :world
+  attr_reader :world, :evolutions_per_second
 
-  def initialize(world = random_world(64, 48), evolutions_per_second = 10, runner_type = GosuRunner)
-    @runner = runner_type.new(world, evolutions_per_second)
+  def initialize(world = random_world(64, 48), evolutions_per_second = 10, runner = GosuRunner)
+    @world, @evolutions_per_second, @runner = world, evolutions_per_second, runner
   end
 
   def run
-    @runner.run
+    @runner.new(world, evolutions_per_second).run
   end
 
   def random_world(width, height)
